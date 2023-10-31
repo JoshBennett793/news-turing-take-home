22cdd8dbc79032b1b75d6332d07e0f4c250847ec
Joshua Bennett <joshbennett793@yahoo.com>
2023-10-31 00:27:54 -0500

    Return mock data and set to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Nav from './Components/Nav/Nav';
+import { getNewsByCategory } from './apiCalls';
 
 function App() {
   const [news, setNews] = useState([]);
 
+  async function getNews(category = 'technology') {
+    try {
+      const data = await getNewsByCategory(category);
+      setNews(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
   return (
     <>
-      <Nav setNews={setNews} />
+      <Nav setNews={getNews} />
     </>
   );
 }
