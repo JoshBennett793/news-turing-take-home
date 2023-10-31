b9e75b10e3bda66dadc67dc4259b437fdb856c23
Joshua Bennett <joshbennett793@yahoo.com>
2023-10-31 11:05:06 -0500

    Add func to get and set latest news

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Nav from './Components/Nav/Nav';
-import { getNewsByCategory } from './apiCalls';
+import { useEffect, useState } from 'react'
+import './App.css'
+import Nav from './Components/Nav/Nav'
+import { getLatestNewsByCategory, getTopHeadlinesByCategory } from './apiCalls'
 
 function App() {
-  const [news, setNews] = useState([]);
+  const [headlines, setHeadlines] = useState([])
+  const [latest, setLatest] = useState([])
 
-  async function getNews(category = 'technology') {
+  async function getHeadlines(category = 'technology') {
     try {
-      const data = await getNewsByCategory(category);
-      setNews(data);
-      console.log(data);
+      const headlines = await getTopHeadlinesByCategory(category)
+      console.log(headlines)
+      return headlines
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
 
+  async function getLatest(category = 'technology') {
+    try {
+      const latest = await getLatestNewsByCategory(category)
+      console.log(latest)
+      return latest
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function setNews(category) {
+    const headlines = await getHeadlines(category)
+    setHeadlines(headlines)
+
+    const latest = await getLatest(category)
+    setLatest(latest)
+  }
+
   useEffect(() => {
-    getNews();
-  }, []);
+    const headlines = getHeadlines()
+    setHeadlines(headlines)
+
+    const latest = getLatest()
+    setLatest(latest)
+  }, [])
 
   return (
     <>
-      <Nav setNews={getNews} />
+      <Nav setNews={setNews} />
+      {/* <Headlines headlines={headlines} /> */}
+      {/* <Latest latest={latest} /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
