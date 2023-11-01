7b5d5c5db6db560eef5db87c6945ce53815a2a30
Joshua Bennett <joshbennett793@yahoo.com>
2023-11-01 14:38:08 -0500

    Render content of article

--- src/Components/SingleArticle/SingleArticle.jsx
+++ src/Components/SingleArticle/SingleArticle.jsx
@@ -8,13 +8,20 @@ export default function SingleArticle() {
   const { title, author } = useParams()
   const { headlines } = useNews()
   const article = findArticle(title, author, headlines)
-  console.log(article);
 
   return (
     <>
       <article className='single-article-container'>
-        <img className='single-article-image' src={article.urlToImage} alt='Image provided by newsapi.org' />
-
+        <img
+          className='single-article-image'
+          src={article.urlToImage}
+          alt='Image provided by newsapi.org'
+        />
+        <h1 className='single-article-title'>{article.title}</h1>
+        <p className='single-article-author'>By {article.author}</p>
+        <p className='single-article-source'>{article.source}</p>
+        <p className='single-article-date'>{article.publishedAt}</p>
+        <p className='single-article-content'>{article.content}</p>
       </article>
     </>
   )
