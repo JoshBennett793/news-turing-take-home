b74a119440e893ff5ab88e4ba17f23a47f41c2c5
Joshua Bennett <joshbennett793@yahoo.com>
2023-11-03 15:46:18 -0500

    Test for correct article content and metadata

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 describe('Main news view', () => {
   beforeEach(() => {
-    const apiKey = 'ae00f1046a14489a8d578e31c95336ff'
-
     cy.intercept(
       'GET',
       'https://newsapi.org/v2/top-headlines?country=us&category=technology&sortBy=popularity&pageSize=20&apiKey=ae00f1046a14489a8d578e31c95336ff',
@@ -56,5 +54,80 @@ describe('Main news view', () => {
     cy.get('.latest-container .latest-articles-container')
       .children()
       .should('have.length', 10)
+  })
+
+  it('Renders correct article information', () => {
+    cy.get('.headlines-container .articles-container')
+      .children()
+      .first()
+      .within(() => {
+        cy.get('img')
+          .should('be.visible')
+          .and(
+            'have.attr',
+            'src',
+            'https://editors.dexerto.com/wp-content/uploads/2022/12/10/Pokemon-Go-Keldeo-Mythic-Blade.jpg'
+          )
+          .and('have.attr', 'alt', 'Image provided by newsapi.org')
+        cy.get('h3').should('be.visible').and('contain', 'New Pokemon Go leaks')
+        cy.get('p')
+          .should('be.visible')
+          .and('contain', 'Pokemon Go players could have')
+      })
+    cy.get('.headlines-container .articles-container')
+      .children()
+      .last()
+      .within(() => {
+        cy.get('img')
+          .should('be.visible')
+          .and(
+            'have.attr',
+            'src',
+            'https://r2.ygorganization.com/2023/10/PigtailsForTheWin.jpg'
+          )
+          .and('have.attr', 'alt', 'Image provided by newsapi.org')
+        cy.get('h3')
+          .should('be.visible')
+          .and('contain', 'Esperade the Smashing Superstar')
+        cy.get('p')
+          .should('be.visible')
+          .and('contain', "It's going to be a blast!")
+      })
+    cy.get('.latest-container .latest-articles-container')
+      .children()
+      .first()
+      .within(() => {
+        cy.get('img')
+          .should('be.visible')
+          .should('be.visible')
+          .and(
+            'have.attr',
+            'src',
+            'https://fdn.gsmarena.com/imgroot/news/23/10/samsung-galaxy-z-flip5-retro-ofic/-952x498w6/gsmarena_00.jpg'
+          )
+          .and('have.attr', 'alt', 'Image provided by newsapi.org')
+        cy.get('h3')
+          .should('be.visible')
+          .and('contain', 'Samsung Galaxy Z Flip5')
+        cy.get('p')
+          .should('be.visible')
+          .and('contain', 'Galaxy Z Flip5 pays homage')
+      })
+    cy.get('.latest-container .latest-articles-container')
+      .children()
+      .last()
+      .within(() => {
+        cy.get('img')
+          .should('be.visible')
+          .should('be.visible')
+          .and(
+            'have.attr',
+            'src',
+            'https://nintendoeverything.com/wp-content/uploads/PlayStation-Portal-Switch.jpg'
+          )
+          .and('have.attr', 'alt', 'Image provided by newsapi.org')
+        cy.get('h3').should('be.visible').and('contain', 'PlayStation Portal')
+        cy.get('p').should('be.visible').and('contain', 'Sony boss Eric Lempel')
+      })
   })
 })
